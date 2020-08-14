eeac122dfac56e4e738d76524cf994fa69b12626
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2020-08-14 16:39:06 +0200

    Add Papermill parameters, Slurm jobscript and export NUMEXPR_NUM_THREADS

---
+++ notebooks/Nevergrad_example/jobscript.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/env bash
+#
+#SBATCH --job-name=geotop_calibration
+#SBATCH --partition regular2
+#SBATCH --time=60:00
+#SBATCH --time-min=40:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=64
+#SBATCH --hint=compute_bound
+#SBATCH --mail-type=ALL
+#SBATCH --exclusive=user
+#SBATCH --verbose
+
+module load gnu8
+spack load python@3.8.3
+export NUMEXPR_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+
+ROOT=$HOME/MHPC-project
+WORKING_DIR=$ROOT/notebooks/Nevergrad_example
+PARAMETERS_FILE=$WORKING_DIR/parameters.yaml
+OUTPUT=$ROOT/output.ipynb
+
+OPTIONS="--no-progress-bar -f $PARAMETERS_FILE --cwd $WORKING_DIR"
+COMMAND="pipenv run papermill $OPTIONS $WORKING_DIR/notebook.ipynb $OUTPUT"
+
+srun $COMMAND
