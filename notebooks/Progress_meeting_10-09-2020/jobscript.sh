53719e8e89bf369e839da743ce3f79af6ea52896
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2020-09-07 20:34:17 +0200

    Update Pipfile and notebook, final draft, to test on Ulysses

---
+++ notebooks/Progress_meeting_10-09-2020/jobscript.sh
@@ -0,0 +1,29 @@
+#! /usr/bin/env bash
+#
+#SBATCH --job-name=mhpc_progress_meeting
+#SBATCH --partition regular2
+#SBATCH --time=1:30:00
+#SBATCH --time-min=1:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --hint=compute_bound
+#SBATCH --mail-type=ALL
+#SBATCH --exclusive=user
+#SBATCH --verbose
+
+module load gnu8
+spack load python@3.8.3
+export NUMEXPR_NUM_THREADS=1
+export NUMEXPR_MAX_THREADS=1
+export OMP_NUM_THREADS=1
+
+ROOT=$HOME/MHPC-project
+WORKING_DIR=$ROOT/notebooks/Progress_meeting_10-09-2020
+PARAMETERS_FILE=$WORKING_DIR/parameters.yaml
+OUTPUT=$ROOT/outputs/notebook.ipynb
+
+OPTIONS="--no-progress-bar -f $PARAMETERS_FILE --cwd $WORKING_DIR"
+COMMAND="pipenv run papermill $OPTIONS $WORKING_DIR/notebook.ipynb $OUTPUT"
+
+srun $COMMAND
