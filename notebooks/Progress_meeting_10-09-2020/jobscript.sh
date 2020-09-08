65484635cd7b32c0728a445381538ada130468b3
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2020-09-08 15:43:04 +0200

    Progress meeting, fixes

--- notebooks/Progress_meeting_10-09-2020/jobscript.sh
+++ notebooks/Progress_meeting_10-09-2020/jobscript.sh
@@ -2,9 +2,8 @@
 #
 #SBATCH --job-name=mhpc_progress_meeting
 #SBATCH --partition regular2
-#SBATCH --time=1:30:00
-#SBATCH --time-min=1:00:00
-#SBATCH --chdir=$HOME/MHPC-project
+#SBATCH --time=10:00
+#SBATCH --time-min=5:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
@@ -19,16 +18,20 @@ export NUMEXPR_NUM_THREADS=1
 export NUMEXPR_MAX_THREADS=1
 export OMP_NUM_THREADS=1
 
-WORKING_DIR=notebooks/Progress_meeting_10-09-2020
-PARAMETERS_FILE=$WORKING_DIR/parameters.yaml
-OUTPUT_DIR=$WORKING_DIR/outputs/$(date +%F_%H-%M)
+ROOT=$HOME/MHPC-project
+WORKING_DIR=$ROOT/notebooks/Progress_meeting_10-09-2020
+OUTPUT_DIR=$WORKING_DIR/data/outputs/$(date +%F_%H-%M)
 
-OPTIONS="--no-progress-bar -p output_path $OUTPUT_DIR -f $PARAMETERS_FILE --cwd $WORKING_DIR"
+OPTIONS="--no-progress-bar -p output_path $OUTPUT_DIR -f $WORKING_DIR/parameters.yaml --cwd $WORKING_DIR"
 
 mkdir -p $OUTPUT_DIR
+
 srun pipenv run papermill $OPTIONS $WORKING_DIR/notebook.ipynb $OUTPUT_DIR/notebook.ipynb
-srun pipenv jupyter nbconvert --to html --output $OUTPUT_DIR/notebook.html $OUTPUT_DIR/notebook.ipynb
-srun pipenv jupyter nbconvert --to slides --output $OUTPUT_DIR/slides.html $OUTPUT_DIR/notebook.ipynb
+
+srun pipenv run jupyter nbconvert --to html $OUTPUT_DIR/notebook.ipynb
+srun pipenv run jupyter nbconvert --to slides $OUTPUT_DIR/notebook.ipynb
 cp $WORKING_DIR/jobscript.sh $OUTPUT_DIR/jobscript.sh
 cp $WORKING_DIR/parameters.yaml $OUTPUT_DIR/parameters.yaml
-tar cfJ $OUTPUT_DIR.tar.xz $OUTPUT_DIR --remove-files
+
+cd $OUTPUT_DIR/..
+tar cfJ $(basename $OUTPUT_DIR).tar.xz $(basename $OUTPUT_DIR) --remove-files
