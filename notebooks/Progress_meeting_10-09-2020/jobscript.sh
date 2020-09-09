9fc71987588d37a412c2f26a67521bb90bcc0945
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2020-09-09 18:48:58 +0200

    Progress meeting final draft

--- notebooks/Progress_meeting_10-09-2020/jobscript.sh
+++ notebooks/Progress_meeting_10-09-2020/jobscript.sh
@@ -20,18 +20,8 @@ export OMP_NUM_THREADS=1
 
 ROOT=$HOME/MHPC-project
 WORKING_DIR=$ROOT/notebooks/Progress_meeting_10-09-2020
-OUTPUT_DIR=$WORKING_DIR/data/outputs/$(date +%F_%H-%M)
-
-OPTIONS="--no-progress-bar -p output_dir $OUTPUT_DIR -f $WORKING_DIR/parameters.yaml --cwd $WORKING_DIR"
+OUTPUT_DIR=$WORKING_DIR/data/outputs
+OPTIONS="--no-progress-bar -f $WORKING_DIR/parameters.yaml --cwd $WORKING_DIR"
 
 mkdir -p $OUTPUT_DIR
-
-srun pipenv run papermill $OPTIONS $WORKING_DIR/notebook.ipynb $OUTPUT_DIR/notebook.ipynb
-
-srun pipenv run jupyter nbconvert --to html $OUTPUT_DIR/notebook.ipynb
-srun pipenv run jupyter nbconvert --to slides $OUTPUT_DIR/notebook.ipynb
-cp $WORKING_DIR/jobscript.sh $OUTPUT_DIR/jobscript.sh
-cp $WORKING_DIR/parameters.yaml $OUTPUT_DIR/parameters.yaml
-
-cd $OUTPUT_DIR/..
-tar cfJ $(basename $OUTPUT_DIR).tar.xz $(basename $OUTPUT_DIR) --remove-files
+srun pipenv run papermill $OPTIONS $WORKING_DIR/notebook.ipynb $OUTPUT_DIR/$(date +%F_%H-%M).ipynb
