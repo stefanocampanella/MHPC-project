dd630b3031835b4f10d2395e85102bec479f76f7
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2020-10-08 11:37:18 +0200

    Add def file and updated jobscript

--- notebooks/jobscript.sh
+++ notebooks/jobscript.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 #SBATCH --partition regular2
 #SBATCH --nodes 2
+#SBATCH --tasks-per-node 1
 #SBATCH --cpus-per-task 32
 #SBATCH --mem-per-cpu 1GB
 #SBATCH --exclusive user
@@ -9,37 +10,26 @@
 #SBATCH --mail-type ALL
 #SBATCH --verbose
 
-module load gnu8
-spack load python@3.8.3
+let "WORKER_NUM=(${SLURM_NTASKS} - 1)"
+let "TOTAL_CORES=${SLURM_NTASKS} * ${SLURM_CPUS_PER_TASK}"
+PORT='6379'
 
-let "worker_num=(${SLURM_NTASKS} - 1)"
-let "total_cores=${SLURM_NTASKS} * ${SLURM_CPUS_PER_TASK}"
-port='6379'
-ip_head=`hostname`:$port
-export ip_head
+export ADDRESS=`hostname`:$PORT
 
-ROOT=$HOME/MHPC-project
-WORKING_DIR=$ROOT/notebooks
-OUTPUT_DIR=$WORKING_DIR/outputs
+INPUT=$1
+OUTPUT=$2
+CONTAINER=$3
+PARAMETERS_FILE=$4
 
-INPUT=$WORKING_DIR/$1.ipynb
-OUTPUT=$OUTPUT_DIR/$1_$(date +%F_%H-%M).ipynb
-OPTIONS="--no-progress-bar --cwd $WORKING_DIR"
-if [[ ! -z "$2" ]]
+singularity instance start $CONTAINER head $PORT ${SLURM_CPUS_PER_TASK}
+
+if [[ $SLURM_JOB_NUM_NODES -gt 1 ]]
 then
-  OPTIONS="$OPTIONS -f $2 -p num_workers total_cores -p address $ip_head "
+  srun --nodes=${WORKER_NUM} --exclude=`hostname` \
+    singularity instance start $CONTAINER worker $ADDRESS ${SLURM_CPUS_PER_TASK}
 fi
 
-export NUMEXPR_NUM_THREADS=1
-export NUMEXPR_MAX_THREADS=1
-export OMP_NUM_THREADS=1
+mkdir -p $(dirname $OUTPUT)
+singularity run $CONTAINER $INPUT $OUTPUT $TOTAL_CORES $ADDRESSS $PARAMETERS_FILE
+singularity instance stop --all
 
-cd $ROOT
-
-srun --nodes=1 --ntasks=1 --cpus-per-task=${SLURM_CPUS_PER_TASK} --nodelist=`hostname` pipenv run ray start --head --dashboard-host 0.0.0.0 --port=$port --num-cpus ${SLURM_CPUS_PER_TASK}
-srun --nodes=${worker_num} --ntasks=${worker_num} --cpus-per-task=${SLURM_CPUS_PER_TASK} --exclude=`hostname` pipenv run ray start --address $ip_head --num-cpus ${SLURM_CPUS_PER_TASK}
-
-mkdir -p $OUTPUT_DIR
-pipenv run papermill $OPTIONS $INPUT $OUTPUT && \
-pipenv run jupyter nbconvert $OUTPUT --to html --output-dir $OUTPUT_DIR && \
-rm $OUTPUT 
