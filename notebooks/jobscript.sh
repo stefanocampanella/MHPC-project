ec1bd473826bcf4890f749b006b66455c1ef36f4
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2020-10-29 10:39:06 +0100

    Prototype using dask

--- notebooks/jobscript.sh
+++ notebooks/jobscript.sh
@@ -11,25 +11,16 @@
 
 module load singularity
 
-PORT=6379
-
 INPUT=$1
 OUTPUT=$2
 CONTAINER=$3
 PARAMETERS_FILE=$4
 
-ray start --head --port=$PORT --num-cpus=$SLURM_CPUS_PER_TASK
+singularity exec $CONTAINER dask-scheduler
 sleep 30
 
-# Implementation using srun, use instead:
-# scontrol show hostnames $SLURM_JOB_NODELIST 
-# to iterate through and ssh into nodes
-if [[ $SLURM_JOB_NUM_NODES -gt 1 ]]
-then
-  srun --nodes=$(($SLURM_NTASKS - 1)) --exclude=`hostname` \
-    ray start --address=`hostname`:$PORT --num-cpus=$SLURM_CPUS_PER_TASK
-  sleep 30
-fi
+srun singularity exec $CONTAINER dask-worker tcp://`hostname`:8786 --nprocs=$SLURM_CPUS_PER_TASK --nthreads=1
+sleep 30
 
 mkdir -p $(dirname $OUTPUT)
 singularity run $CONTAINER $INPUT $OUTPUT $TOTAL_CORES $ADDRESS $PARAMETERS_FILE
