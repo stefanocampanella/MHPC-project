5f06fdc775e5d62ee2ea6d3440c399daa7e00a08
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2021-01-15 09:25:38 +0100

    add convergence_plot

--- mhpc_project/calibration.py
+++ mhpc_project/calibration.py
@@ -1,30 +1,13 @@
 import nevergrad as ng
 import numpy as np
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 from dask.distributed import as_completed
-from tqdm.auto import tqdm
 
 from .comparators import KGE
 from .utils import comparison_plot
 
-
-# The objective function must be a callable object
-# which can be serialized using cloudpickle by Dask.
-# class ObjectiveFunction:
-#
-#     def __init__(self, model, observations):
-#         self.model = model
-#         self.comparators = {}
-#         for target in observations.columns:
-#             self.comparators[target] = KGE(observations[target])
-#
-#     def __call__(self, candidate):
-#         predictions = self.model(*candidate.args, **candidate.kwargs)
-#
-#         square_loss = sum(comp(predictions[target]) ** 2 for target, comp
-#                           in self.comparators.items() if target in predictions)
-#
-#         return candidate, np.sqrt(square_loss)
 
 def objective_function(model, comparators, candidate):
     sim = model(*candidate.args, **candidate.kwargs)
@@ -44,47 +27,43 @@ class Calibration:
         self.comparators = {col: KGE(self.observations[col]) for col in self.observations.columns}
         self.log = []
 
-    def __call__(self, algorithm, budget, num_workers, client, show_progress=False):
-        # objective_function = ObjectiveFunction(self.model, self.observations)
+    def __call__(self, algorithm, budget, num_workers, client):
         optimizer_class = ng.optimizers.registry[algorithm]
         optimizer = optimizer_class(self.parameters.instrumentation,
                                     budget=np.inf,
                                     num_workers=num_workers)
 
-        with tqdm(total=budget, disable=not show_progress) as progress_bar:
-            while optimizer.num_tell < budget:
-                # asked_points = [optimizer.ask() for _ in range(popsize)]
-                # To avoid annoyances with argument unpacking,
-                # ensure that loss takes just one argument.
-                # The best option is passing (and return) the whole candidate object.
-                # Otherwise, because as_completed change the order of the inputs,
-                # one should keep track of the candidates to which the losses belong.
-                client.scatter(self.model, broadcast=True)
-                client.scatter(self.comparators, broadcast=True)
-                # futures = client.map(objective_function, asked_points)
-                futures = [client.submit(objective_function, self.model, self.comparators, optimizer.ask())
-                           for _ in range(num_workers)]
-                completed_queue = as_completed(futures)
-                for batch in completed_queue.batches():
-                    for future in batch:
-                        if future.status == 'finished':
-                            result = future.result()
-                            optimizer.tell(*result)
-                            self.log.append(result)
-                            progress_bar.update()
-                        else:
-                            # See the executor.map comment
-                            # new_future = client.submit(objective_function, optimizer.ask())
-                            new_future = client.submit(objective_function,
-                                                       self.model,
-                                                       self.comparators,
-                                                       optimizer.ask())
-                            completed_queue.add(new_future)
+        while optimizer.num_tell < budget:
+            client.scatter(self.model, broadcast=True)
+            client.scatter(self.comparators, broadcast=True)
+            futures = [client.submit(objective_function, self.model, self.comparators, optimizer.ask())
+                       for _ in range(num_workers)]
+            completed_queue = as_completed(futures)
+            for batch in completed_queue.batches():
+                for future in batch:
+                    if future.status == 'finished':
+                        candidate, loss = future.result()
+                        optimizer.tell(candidate, loss)
+                        self.log.append((candidate.generation, loss))
+                    else:
+                        new_future = client.submit(objective_function,
+                                                   self.model,
+                                                   self.comparators,
+                                                   optimizer.ask())
+                        completed_queue.add(new_future)
 
-        recommendation = optimizer.provide_recommendation()
-        predictions = self.model(*recommendation.args, **recommendation.kwargs)
+        return optimizer.provide_recommendation()
 
-        return predictions, recommendation
+    def convergence_plot(self, figsize=(16, 9), dpi=100):
+        max_generation_number = max(n for n, _ in self.log)
+        min_losses = np.fromiter([min(l for n, l in self.log if n <= k)
+                                  for k in range(1, max_generation_number + 1)], dtype=float)
+        data = pd.DataFrame(self.log, columns=['generation', 'loss'])
+        figure, axes = plt.subplots(figsize=figsize, dpi=dpi)
+        sns.lineplot(data=data, x='generation', y='loss', ax=axes)
+        sns.lineplot(x=range(1, max_generation_number + 1), y=min_losses, ax=axes)
+
+        return figure
 
     def comparison_plots(self, candidate, **kwargs):
         predictions = self.model(*candidate.args, **candidate.kwargs)
