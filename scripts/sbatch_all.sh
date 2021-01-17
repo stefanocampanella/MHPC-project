aeb05f912be63b5e6578c84c7aee2a750d3e6203
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2021-01-17 16:00:56 +0100

    SBATCH 'EM ALL

--- scripts/sbatch_all.sh
+++ scripts/sbatch_all.sh
@@ -1,22 +1,48 @@
 #! /usr/bin/env bash
 
-ALGORITHM=$1
-POPSIZE=$2
-NUM_GENERATIONS=$3
-TIMEOUT=$4
-PARTITION=$5
-NUM_NODES=$6
-TIME=$7
-
 export MHPCPROJECT_ROOT=/scratch/$USER/MHPC-project
 export PYTHONPATH=$MHPCPROJECT_ROOT
 export DASK_CONFIG=$MHPCPROJECT_ROOT/config/dask
 
+TIMEOUT=180
+REPETITIONS=3
+OUTPUT="$MHPCPROJECT_ROOT/runs"
+
 cd "$MHPCPROJECT_ROOT" || exit
-for SITE in DOMEF DOMES DOPAS Kaltern Latsch "Matsch B2" "Matsch P2" NEPAS
+for ALGORITHM in NGO PSO Random
 do
-  sbatch -p "$PARTITION" -J "$SITE" -N "$NUM_NODES" -t "$TIME" \
-    ./scripts/run.slurm "$SITE" \
-    "$ALGORITHM" "$POPSIZE" "$NUM_GENERATIONS" "$TIMEOUT" \
-    "/scratch/$USER/MHPC-project_outputs"
+  for POPSIZE in 64 128 256 512
+  do
+    for NUM_GENERATIONS in 8 16 32 64
+    do
+      for NUM_NODES in 4 8 12 16 20 24 28 32
+      do
+        for SITE in DOMEF DOMES DOPAS Kaltern Latsch "Matsch B2" "Matsch P2" NEPAS
+        do
+          SECONDS=$((60 * NUM_GENERATIONS * POPSIZE / NUM_NODES))
+          if [[ $SECONDS -gt 43200 ]]
+          then
+            PARTITION=long2
+          elif [[ $NUM_NODES -gt 16 ]]
+          then
+            PARTITION=wide2
+          elif [[ $SECONDS -gt 43200 ]] && [[ $NUM_NODES -gt 16 ]]
+          then
+            PARTITION=""
+          else
+            PARTITION=regular2
+          fi
+          if [[ -n $PARTITION ]]
+          then
+            TIME=$(date -d@$SECONDS -u "+%H:%M:%S")
+            for n in $(seq $REPETITIONS)
+            do
+              sbatch -p "$PARTITION" -J "$SITE" -N "$NUM_NODES" -t "$TIME" \
+                ./scripts/run.slurm "$SITE" "$ALGORITHM" "$POPSIZE" "$NUM_GENERATIONS" "$TIMEOUT" "$OUTPUT"
+            done
+          fi
+        done
+      done
+    done
+  done
 done
