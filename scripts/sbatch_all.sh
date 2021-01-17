3742de2631bdf6b9f82462bb915ee8acc2edaf4a
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2021-01-17 11:28:14 +0100

    add sbatch_all.sh

--- scripts/sbatch_all.sh
+++ scripts/sbatch_all.sh
@@ -4,8 +4,9 @@ ALGORITHM=$1
 POPSIZE=$2
 NUM_GENERATIONS=$3
 TIMEOUT=$4
-NUM_NODES=$5
-TIME=$6
+PARTITION=$5
+NUM_NODES=$6
+TIME=$7
 
 export MHPCPROJECT_ROOT=/scratch/$USER/MHPC-project
 export PYTHONPATH=$MHPCPROJECT_ROOT
@@ -14,7 +15,7 @@ export DASK_CONFIG=$MHPCPROJECT_ROOT/config/dask
 cd "$MHPCPROJECT_ROOT" || exit
 for SITE in DOMEF DOMES DOPAS Kaltern Latsch "Matsch B2" "Matsch P2" NEPAS
 do
-  sbatch -J "$SITE" -N "$NUM_NODES" -t "$TIME" \
+  sbatch -p "$PARTITION" -J "$SITE" -N "$NUM_NODES" -t "$TIME" \
     ./scripts/calibration "$SITE" \
     "$ALGORITHM" "$POPSIZE" "$NUM_GENERATIONS" "$TIMEOUT" \
     "/scratch/$USER/MHPC-project_outputs"
