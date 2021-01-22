320dbae6689c501497f321bbdaa5d547de08b9f8
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2021-01-22 17:41:29 +0100

    changes

--- scripts/sbatch_tests.sh
+++ scripts/sbatch_tests.sh
@@ -9,29 +9,25 @@ mkdir -p "slurm_outputs"
 
 echo "==== Submitting test jobs ===="
 SITE=testbed
-TIMEOUT=180
+TIMEOUT=300
 PARAMETERS_PATH="$MHPCPROJECT_ROOT/data/parameters/testbed.csv"
 ALGORITHM=NGO
 POPSIZE=64
-NUM_GENERATIONS=8
+NUM_GENERATIONS=4
 OUTPUT="$MHPCPROJECT_ROOT/runs/scaling"
 for NUM_NODES in  4 8 16 32
 do
-  if [[ $NUM_NODES -eq 2 ]]
+  if [[ $NUM_NODES -eq 4 ]]
   then
     PARTITION=regular2
-    TIME=6:00:00
-  elif [[ $NUM_NODES -eq 4 ]]
-  then
-    PARTITION=regular2
-    TIME=3:00:00
+    TIME=2:00:00
   elif [[ $NUM_NODES -le 16 ]]
   then
     PARTITION=regular2
-    TIME=1:30:00
+    TIME=1:00:00
   else
     PARTITION=wide2
-    TIME=1:30:00
+    TIME=1:00:00
   fi
   sbatch -p "$PARTITION" -J "scaling_$SITE" -N "$NUM_NODES" -t "$TIME" \
          -o "slurm_outputs/%x-%j.out"  -e "slurm_outputs/%x-%j.err" \
